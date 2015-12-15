a1a1612b17614a93696b4f5b25e9d8daf9e45f19
Kyle Fuller <kyle@fuller.li>
2015-12-15 01:31:57 +0000

    [Package] Update to Commander 0.4

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0, minor: 2),
     .Package(url: "https://github.com/nestproject/Inquiline.git", majorVersion: 0, minor: 2),
-    .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 3),
+    .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 4),
   ]
 )

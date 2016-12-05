addf9696adc068b3ced19c4f5692a772cc085c1f
Kyle Fuller <kyle@fuller.li>
2016-12-05 23:50:36 +0000

    chore: Support to Commanders VariadicOption

--- Sources/Curassow/Curassow.swift
+++ Sources/Curassow/Curassow.swift
@@ -9,53 +9,6 @@ import Commander
 import Inquiline
 
 
-extension ArgumentConvertible {
-  init(string: String) throws {
-    try self.init(parser: ArgumentParser(arguments: [string]))
-  }
-}
-
-
-class MultiOption<T : ArgumentConvertible> : ArgumentDescriptor {
-  typealias ValueType = [T]
-
-  let name: String
-  let flag: Character?
-  let description: String?
-  let `default`: ValueType
-  var type: ArgumentType { return .option }
-
-  init(_ name: String, _ default: ValueType, flag: Character? = nil, description: String? = nil) {
-    self.name = name
-    self.flag = flag
-    self.description = description
-    self.`default` = `default`
-  }
-
-  func parse(_ parser: ArgumentParser) throws -> ValueType {
-    var options: ValueType = []
-
-    while let value = try parser.shiftValueForOption(name) {
-      let value = try T(string: value)
-      options.append(value)
-    }
-
-    if let flag = flag {
-      while let value = try parser.shiftValueForFlag(flag) {
-        let value = try T(string: value)
-        options.append(value)
-      }
-    }
-
-    if options.isEmpty {
-      return `default`
-    }
-
-    return options
-  }
-}
-
-
 func getIntEnv(_ key: String, default: Int) -> Int {
   let value = getenv(key)
   if value != nil {
@@ -84,7 +37,7 @@ public func serve(_ closure: @escaping (RequestType) -> ResponseType) -> Never  
   command(
     Option("worker-type", "sync"),
     Option("workers", workers, description: "The number of processes for handling requests."),
-    MultiOption("bind", [Address.ip(hostname: "0.0.0.0", port: port)], description: "The address to bind sockets."),
+    VariadicOption("bind", [Address.ip(hostname: "0.0.0.0", port: port)], description: "The address to bind sockets."),
     Option("timeout", 30, description: "Amount of seconds to wait on a worker without activity before killing and restarting the worker."),
     Flag("daemon", description: "Detaches the server from the controlling terminal and enter the background.")
   ) { workerType, workers, addresses, timeout, daemonize in
